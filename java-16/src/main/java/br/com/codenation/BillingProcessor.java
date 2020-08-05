70ff82baaf31f88da04e11c1323944db0369877b
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-08-05 15:08:25 -0300

    Refatorando Formas de Pagamento

---
+++ java-16/src/main/java/br/com/codenation/BillingProcessor.java
@@ -0,0 +1,12 @@
+package br.com.codenation;
+
+public class BillingProcessor {
+
+    public Double calculate(Order order) {
+        try {
+            return order.getPaymentMethod().getPaymentStrategy().calculate(order.getPrice());
+        } catch (Exception e) {
+            throw new RuntimeException("Payment method not implemented");
+        }
+    }
+}
